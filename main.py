adff9a2bdd170eb4b0b134cac6ac8dc4e228a608
Vince Tóth <vincetot97@gmail.com>
2021-01-11 10:04:32 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def gabi_a(cheer):
+    for x in range(cheer):
+        print("gabi")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    c=input("Gimme a number")
+    gabi_a(int(c))
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
